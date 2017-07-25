6b0b978638d47bb0a30869a48196a9c10e924c6c
Alfredo Sánchez Alberca <asalber@gmail.com>
2017-07-25 19:55:06 +0200

    Some changes in charts and adde examples of non-normal distributions.

--- img/descriptive_charts.R
+++ img/descriptive_charts.R
@@ -6,6 +6,15 @@ library(plyr)
 library(plotly)
 require(Hmisc)
 
+# Colors
+blueceu <- rgb(5,161,230,255,maxColorValue = 255) #0099CC
+blueceulight <- rgb(137,211,243,255,maxColorValue = 255) #89D3F3
+redceu <- rgb(238,50,36,255,maxColorValue = 255) #FF3333
+greenceu <- rgb(194,205,255,255,maxColorValue = 255) #CCCC33
+orangeceu <- rgb(248,131,121,255,maxColorValue = 255)#F88379
+purpleceu <- rgb(169,78,145,255,maxColorValue = 255) #996699
+greyceu <- rgb(117,117,97,255,maxColorValue = 255) #666666
+
 options(tikzDefaultEngine = "pdftex")
 # Bar chart
 data <- read.table("data/children_cars_data.csv", header=TRUE, sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
@@ -13,7 +22,7 @@ tab <- table(data$children)
 # Absolute frequency bar chart of number of children
 tikz(file="img/descriptive/abs_freq_bar_chart_polygon.tex", width=7, height=5)
 par(cex.lab=1.2)
-barplot(tab,space=0.5, main="Absolute frequency distribution of number of children", xlab="Number of children", ylab="Absolute frequency $n_i$", col="coral")
+barplot(tab,space=0.5, main="Absolute frequency distribution of number of children", xlab="Number of children", ylab="Absolute frequency $n_i$", col=blueceulight)
 lines(c(1,2.5,4,5.5,7),tab,col="royalblue1",lwd=3)
 dev.off()
 
@@ -35,7 +44,7 @@ layout(p,title="Relative frequency distribution of number of children", xaxis=li
 tikz(file="img/descriptive/cum_abs_freq_bar_chart_polygon.tex", width=7, height=5)
 tabcum <- cumsum(tab)
 par(cex.lab=1.2)
-barplot(tabcum, space=0.5, main="Cumulative absolute frequency distribution of number of children", xlab="Number of children", ylab="Cumulative absolute frequency $N_i$", col="coral")
+barplot(tabcum, space=0.5, main="Cumulative absolute frequency distribution of number of children", xlab="Number of children", ylab="Cumulative absolute frequency $N_i$", col=blueceulight)
 lines(c(0,1,1,2.5,2.5,4,4,5.5,5.5,7,7,8.5),c(0,0,2,2,8,8,22,22,24,24,25,25),col="royalblue", lwd=3)
 dev.off()
 
@@ -56,8 +65,8 @@ library(agricolae)
 # Absolute frequency histogram of height
 tikz(file="img/descriptive/abs_freq_histogram_polygon.tex", width=7, height=5)
 par(cex.lab=1.2)
-(h <- hist(data$height, ylim=c(0,12), main="Absolute frequency distribution of Height", xlab="Height (cm)", ylab="Absolute frequency $n_i$", col="coral"))
-polygon.freq(h,frequency=1,col="royalblue",lwd=3)
+(h <- hist(data$height, ylim=c(0,12), main="Absolute frequency distribution of Height", xlab="Height (cm)", ylab="Absolute frequency $n_i$", col=blueceulight))
+polygon.freq(h,frequency=1,col=blueceulight,lwd=3)
 dev.off()
 
 p<-plot_ly(x=data[["height"]],type="histogram", marker=list(line=list(width=1)))
@@ -74,9 +83,9 @@ tikz(file="img/descriptive/cum_abs_freq_histogram_polygon.tex", width=7, height=
 par(cex.lab=1.2)
 h <- hist(data$height, plot=FALSE)
 h$counts <- cumsum(h$counts)
-plot(h, main="Cumulative absolute frequency distribution of Height", xlab="Height (cm)", ylab="Cumulative absolute frequency $N_i$", col="coral")
+plot(h, main="Cumulative absolute frequency distribution of Height", xlab="Height (cm)", ylab="Cumulative absolute frequency $N_i$", col=blueceulight)
 yy <- c(0, h$counts)
-lines(h$breaks, yy, lwd=3, col = "royalblue") 
+lines(h$breaks, yy, lwd=3, col = blueceulight) 
 dev.off()
 
 # Cumulative relative frequency histogram of height
@@ -84,9 +93,9 @@ tikz(file="img/descriptive/cum_rel_freq_histogram_polygon.tex", width=7, height=
 par(cex.lab=1.2)
 h <- hist(data$height, plot=FALSE)
 h$counts <- cumsum(h[["counts"]])/sum(h[["counts"]])
-plot(h, main="Cumulative relative requency distribution of Height", xlab="Height (cm)", ylab="Cumulative relative frequency $F_i$", col="coral")
+plot(h, main="Cumulative relative requency distribution of Height", xlab="Height (cm)", ylab="Cumulative relative frequency $F_i$", col=blueceulight)
 yy <- c(0, h[["counts"]])
-lines(h$breaks, yy, lwd=3, col = "royalblue") 
+lines(h$breaks, yy, lwd=3, col = blueceulight) 
 dev.off()
 
 h <- hist(data$height, plot=FALSE)
@@ -101,15 +110,15 @@ options(digits=1)
 par(cex.lab=1.2)
 out <- histbackback(split(data$height, data$sex), xlim=c(-10,10), main = 'Frequency distribution histogram of Height by Gender', xlab=c("male", "female"), ylab="Height")
 abline(v= (-8:8)*2 , col ="gray" , lty =3) 
-barplot(-out$left, col="coral" , horiz=TRUE, space=0, add=TRUE, axes=FALSE) 
-barplot(out$right, col="royalblue1", horiz=TRUE, space=0, add=TRUE, axes=FALSE) 
+barplot(-out$left, col=blueceulight , horiz=TRUE, space=0, add=TRUE, axes=FALSE) 
+barplot(out$right, col=orangeceu, horiz=TRUE, space=0, add=TRUE, axes=FALSE) 
 dev.off()
 
 # Box and whiskers plot by sex
 tikz(file="img/descriptive/factor_box_plot.tex", width=5, height=5)
 options(digits=2)
 par(cex.lab=1.2)
-boxplot(height~sex, main="Frequency distribution box plot of Height by Gender", ylab="Height", xlab="Gender", ylim=c(150,200), labels=c("Male","Female"), col=c("coral","royalblue1"), data=data)
+boxplot(height~sex, main="Frequency distribution box plot of Height by Gender", ylab="Height", xlab="Gender", ylim=c(150,200), labels=c("Male","Female"), col=c(blueceulight,orangeceu), data=data)
 dev.off()
 
 # Pie chart
@@ -121,7 +130,7 @@ labels <- labels(tab)[[1]]
 pctg <- round(tab/sum(tab)*100,2)
 labels <- paste(labels, pctg) # add percents to labels 
 labels <- paste(labels,"\\%",sep="") # ad % to labels 
-pie(tab, main="Relative frequency distribution of blood types", labels=labels, col=c("lightgreen", "coral", "royalblue", "brown"))
+pie(tab, main="Relative frequency distribution of blood types", labels=labels, col=c(greenceu, redceu, blueceu, "brown"))
 dev.off()
 
 freq<-count(data,'blood.type.child')
@@ -132,7 +141,7 @@ layout(p,title="Relative frequency distribution of blood types", autosize=FALSE,
 data <- read.table("data/newborn_weights.csv", header=TRUE, sep=" ", na.strings="NA", dec=".", strip.white=TRUE)
 tikz(file="img/descriptive/boxplot.tex", width=7, height=5)
 par(cex.lab=1.2)
-boxplot(data$weight, horizontal=TRUE, col="coral", pch=16, main="Box plot of newborn weights", xlab="Weight (Kg)")
+boxplot(data$weight, horizontal=TRUE, col=blueceulight, pch=16, main="Box plot of newborn weights", xlab="Weight (Kg)")
 text(2.9,0.75,expression(italic(Q[1])))
 text(3.1,0.75,expression(italic(Q[2])))
 text(3.4,0.75,expression(italic(Q[3])))
@@ -146,7 +155,7 @@ dev.off()
 data <- read.table("data/children_cars_data.csv", header=TRUE, sep="\t", na.strings="NA", dec=".", strip.white=TRUE)
 tikz(file="img/descriptive/boxplot_children.tex", width=7, height=5)
 par(cex.lab=1.2)
-boxplot(data$children, horizontal=TRUE, col="coral", pch=16, main="Box plot of number of children", xlab="Number of children")
+boxplot(data$children, horizontal=TRUE, col=blueceulight, pch=16, main="Box plot of number of children", xlab="Number of children")
 dev.off()
 
 # Symmetrical histrogram
@@ -155,10 +164,10 @@ m <- mean(x)
 tikz(file="img/descriptive/symmetrical_distribution.tex", width=7, height=5)
 layout(matrix(c(1,2),2,1),heights=c(3,1))
 par(mai=c(0,1,0.5,0),cex.lab=1.2)
-hist(x,xlim=c(-3.5,3.5),breaks=50, ylim=c(0,0.4), xaxt="n", prob=T, main="Symmetrical distribution $g1=0$", xlab ="", ylab="Relative frequency", col="coral")
+hist(x,xlim=c(-3.5,3.5),breaks=50, ylim=c(0,0.4), xaxt="n", prob=T, main="Symmetrical distribution $g1=0$", xlab ="", ylab="Relative frequency", col=blueceulight)
 axis(1,at=m,labels="$\\bar x=Me$")
 par(mai=c(0,1,0,0))
-boxplot(x,horizontal=TRUE,axes=FALSE, outline=FALSE, col="coral")
+boxplot(x,horizontal=TRUE,axes=FALSE, outline=FALSE, col=blueceulight)
 dev.off()
 
 # left-skewed histogram
@@ -168,20 +177,20 @@ me <- median(x)
 tikz(file="img/descriptive/left_skewed_distribution.tex", width=7, height=5)
 layout(matrix(c(1,2),2,1),heights=c(3,1))
 par(mai=c(0,1,0.5,0),cex.lab=1.2)
-hist(-x, xaxt="n", breaks=700, xlim=c(-5,0), prob=T, main="Left-skewed distribution $g_1<0$", ylab="Relative frequency", col="coral")
+hist(-x, xaxt="n", breaks=700, xlim=c(-5,0), prob=T, main="Left-skewed distribution $g_1<0$", ylab="Relative frequency", col=blueceulight)
 axis(1,at=c(-m,-me),labels=c("$\\bar x$","$Me$"))
 par(mai=c(0,1,0,0))
-boxplot(-x, horizontal=TRUE, ylim=c(-5,0), axes=FALSE, outline=FALSE, col="coral")
+boxplot(-x, horizontal=TRUE, ylim=c(-5,0), axes=FALSE, outline=FALSE, col=blueceulight)
 dev.off()
 
 # right-skewed histogram
 tikz(file="img/descriptive/right_skewed_distribution.tex", width=7, height=5)
 layout(matrix(c(1,2),2,1),heights=c(3,1))
 par(mai=c(0,1,0.5,0),cex.lab=1.2)
-hist(x, xaxt="n", breaks=700, xlim=c(0,5), prob=T, main="Right-skewed distribution $g_1>0$", ylab="Relative frequency", col="coral")
+hist(x, xaxt="n", breaks=700, xlim=c(0,5), prob=T, main="Right-skewed distribution $g_1>0$", ylab="Relative frequency", col=blueceulight)
 axis(1,at=c(m,me),labels=c("$\\bar x$","$Me$"))
 par(mai=c(0,1,0,0))
-boxplot(x, horizontal=TRUE, ylim=c(0,5), axes=FALSE, outline=FALSE, col="coral")
+boxplot(x, horizontal=TRUE, ylim=c(0,5), axes=FALSE, outline=FALSE, col=blueceulight)
 dev.off()
 
 # mesokurtic distribution
@@ -189,10 +198,10 @@ par(cex.lab=1.2)
 x <- rnorm(1000000)
 m <- mean(x)
 tikz(file="img/descriptive/mesokurtic_distribution.tex", width=7, height=5)
-hist(x,xlim=c(-3.5,3.5), breaks=50, ylim=c(0,0.4),prob=T, main="Mesokurtic distribution $g_2=0$" , xlab ="", ylab="Relative frequency", col="coral", axes=F)
+hist(x,xlim=c(-3.5,3.5), breaks=50, ylim=c(0,0.4),prob=T, main="Mesokurtic distribution $g_2=0$" , xlab ="", ylab="Relative frequency", col=blueceulight, axes=F)
 axis(1,at=m,labels="$\\bar x$")
 axis(2)
-curve(dnorm, from=-3.5, to=3.5, add=TRUE, col="royalblue", lwd=2) 
+curve(dnorm, from=-3.5, to=3.5, add=TRUE, col=redceu, lwd=2) 
 dev.off()
 
 
@@ -204,11 +213,11 @@ x  <- x1+x2+x3
 m <- mean(x)
 tikz(file="img/descriptive/platykurtic_distribution.tex", width=7, height=5)
 par(cex.lab=1.2)
-hist(x, prob=T, breaks=50, ylim=c(0,0.2), main="Platykurtic distribution $g_2<0$", xlab ="", ylab="Relative frequency", col="coral", axes=F)
+hist(x, prob=T, breaks=50, ylim=c(0,0.2), main="Platykurtic distribution $g_2<0$", xlab ="", ylab="Relative frequency", col=blueceulight, axes=F)
 axis(1,at=m,labels="$\\bar x$")
 axis(2)
 f <- function (z){return(dnorm(z,mean=0,sd=2.5))}
-curve(f, add=TRUE, col="royalblue", lwd=2)
+curve(f, add=TRUE, col=redceu, lwd=2)
 dev.off()
 
 # leptokurtic ditribution
@@ -216,24 +225,24 @@ x <- rt(1000000,4)
 m <- mean(x)
 tikz(file="img/descriptive/leptokurtic_distribution.tex", width=7, height=5)
 par(cex.lab=1.2)
-hist(x, prob=T, breaks=seq(-100,100,by=0.2), xlim=c(-4.1,4.1), ylim=c(0,0.4), main="Leptokurtic distribution $g_2>0$", xlab ="", ylab="Relative frequency", col="coral", axes=F)
+hist(x, prob=T, breaks=seq(-100,100,by=0.2), xlim=c(-4.1,4.1), ylim=c(0,0.4), main="Leptokurtic distribution $g_2>0$", xlab ="", ylab="Relative frequency", col=blueceulight, axes=F)
 axis(1,at=m,labels="$\\bar x$")
 axis(2)
 f <- function (z){return(dnorm(z,mean=0,sd=sqrt(2)))}
-curve(f, add=TRUE, col="royalblue", lwd=2)
+curve(f, add=TRUE, col=redceu, lwd=2)
 dev.off()
 
 # right-skewed histogram
 x <- rf(1000000,8,8)
 tikz(file="img/descriptive/right_skewed_histogram.tex", width=5, height=5)
 par(mai=c(0,0,0,0))
-hist(x, xaxt="n", breaks=seq(0,max(x)+1,by=0.14), xlim=c(0,5), prob=TRUE, main="", col="coral", axes=FALSE)
+hist(x, xaxt="n", breaks=seq(0,max(x)+1,by=0.14), xlim=c(0,5), prob=TRUE, main="", col=blueceulight, axes=FALSE)
 dev.off()
 
 # Symmetrical histogram whithout box plot
 x <- rnorm(1000000)
 tikz(file="img/descriptive/symmetrical_histogram.tex", width=5, height=5)
 par(mai=c(0,0,0,0))
-hist(x, breaks=seq(min(x),max(x)+1,by=0.2), xlim=c(-3.5,3.5), ylim=c(0,0.4), prob=TRUE, main="", col="coral", axes=FALSE)
+hist(x, breaks=seq(min(x),max(x)+1,by=0.2), xlim=c(-3.5,3.5), ylim=c(0,0.4), prob=TRUE, main="", col=blueceulight, axes=FALSE)
 dev.off()
 
--- img/distribution_shapes.R
+++ img/distribution_shapes.R
@@ -4,6 +4,15 @@
 setwd("/media/alf/datos/drive/CEU/DOCENCIA/materiales/estadistica/presentaciones/statistics_manual")
 library(tikzDevice)
 
+# Colors
+blueceu <- rgb(5,161,230,255,maxColorValue = 255) #0099CC
+blueceulight <- rgb(137,211,243,255,maxColorValue = 255) #89D3F3
+redceu <- rgb(238,50,36,255,maxColorValue = 255) #FF3333
+greenceu <- rgb(194,205,255,255,maxColorValue = 255) #CCCC33
+orangeceu <- rgb(248,131,121,255,maxColorValue = 255)#F88379
+purpleceu <- rgb(169,78,145,255,maxColorValue = 255) #996699
+greyceu <- rgb(117,117,97,255,maxColorValue = 255) #666666
+
 # Right-skewed distribution
 tikz(file="img/descriptive/right_skewed_distribution_example.tex", width=7, height=5)
 par(cex.lab=1.2)
@@ -11,7 +20,7 @@ income <- seq(2500,207500,5000)/1000
 counts <- c(4235, 4071, 6324, 6470, 6765, 6222, 6354, 5743, 5203, 5002, 5078, 4140, 4367, 3733, 3683, 3650, 3354, 2893, 2850, 2452, 2792, 2232, 2158, 1748, 1987, 1675, 1474, 1380, 1220, 1111, 1313, 1017, 993, 761, 822, 700, 677, 536, 561, 431, 314, 215)
 breaks <- seq(0,210000,5000)/1000
 data <- rep(income, counts)
-hist(data, main="USA househod income dristribution", breaks=breaks, xaxt="n", xlab="Yearly income (thousand dollars)", ylab="Relative frequency", col="orange", prob=T)
+hist(data, main="USA househod income dristribution", breaks=breaks, xaxt="n", xlab="Yearly income (thousand dollars)", ylab="Relative frequency", col=blueceulight, prob=T)
 axis(side=1, at=breaks, labels=breaks, las=2)
 dev.off()
 
@@ -21,7 +30,7 @@ par(cex.lab=1.2)
 counts <- c(65, 116, 69, 78, 319, 501, 633, 655, 848, 1226, 1633, 2459, 3375, 4669, 6152, 7436, 9526, 12619, 12455, 7113, 2104)
 breaks <- seq(0,100,5)
 data <- rep(breaks, counts)
-hist(data, main="Distribution of age dead of australian males",  xlab="Age of dead", ylab="Relative frequency", breaks = breaks, xaxt="n", col="orange", prob=T)
+hist(data, main="Distribution of age at death of australian males",  xlab="Age of dead", ylab="Relative frequency", breaks = breaks, xaxt="n", col=blueceulight, prob=T)
 axis(side=1, at=breaks, labels=breaks)
 dev.off()
 
@@ -32,6 +41,6 @@ counts <- c(35, 20, 18, 48, 75, 67, 43, 22, 14, 21, 23, 47, 63, 44, 25, 15)
 breaks <- seq(8.5,23.5,1)
 data <- rep(breaks, counts)
 breaks <- seq(8,24)
-hist(data, main="Distribution of clients arrival times in a restaurant",  xlab="Time", ylab="Relative frequency", breaks = breaks, xaxt="n", col="orange", prob=T)
+hist(data, main="Distribution of clients arrival times in a restaurant",  xlab="Time", ylab="Relative frequency", breaks = breaks, xaxt="n", col=blueceulight, prob=T)
 axis(side=1, at=breaks, labels=breaks)
 dev.off()
